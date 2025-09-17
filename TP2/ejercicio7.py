377c736a54cd6b4d00180856a4bdb58e151b1403
Elisa-Correa <elisacorrea2271@gmail.com>
2025-09-17 20:34:00 -0300

    TP 2 - ejercicio 7

---
+++ TP2/ejercicio7.py
@@ -0,0 +1,28 @@
+def intercalar(lista1: list[int], lista2: list[int]) -> list[int]:
+    """
+    Pre: Recibe 2 listas de numeros entero.
+         Permite que sean vacias []
+         Permite que tengan diferente longitud cada una
+
+
+    Recibe 2 listas de numeros enteros, y las intercala guardandolas en una sola lista.
+    Retorna una lista de numeros intercalados.
+    El slicing no tiene problema si las 2 listas no tienen la misma longitud.
+    """
+
+    for i, valor in enumerate(lista2):
+        ind = i*2+1
+        lista1[ind:ind] = [valor]
+
+    return lista1
+
+
+lista1 = []
+lista2 = []
+
+print(intercalar(lista1, lista2))
+
+assert intercalar([8, 1, 3], [5, 9, 7]) == [8, 5, 1, 9, 3, 7]
+assert intercalar([1, 3, 5], [2, 4, 6]) == [1, 2, 3, 4, 5, 6]
+assert intercalar([1, 3, 5, 7, 9], [2, 4, 6]) == [1, 2, 3, 4, 5, 6, 7, 9]
+assert intercalar([1, 3, 5], [2, 4, 6, 8, 10]) == [1, 2, 3, 4, 5, 6, 8, 10]
