aa2e8437b589300351859dc6d89be998a16a2204
Elisa-Correa <elisacorrea2271@gmail.com>
2025-09-17 20:28:16 -0300

    TP 2 - ejercicio 6

---
+++ TP2/ejercicio6.py
@@ -0,0 +1,20 @@
+def normalizar(lista: list[int]) -> list[float]:
+    """
+    Pre: Recibe una lista de numeros enteros como parametro.
+
+    Recibe una lista de enteros, y retorna una lista de flotantes.
+    Realiza la suma de todos los elemntos de la lista, y luego divide cada elemento por el total de la suma.
+    Esto indica la proporciion que representan del total, dando siempre la suma de toda la proporcion 1.0.
+    """
+
+    return [n/sum(lista) for n in lista]
+
+
+print(normalizar([6, 2, 2]))
+
+assert normalizar([1, 1, 2]) == [0.25, 0.25, 0.50]
+assert normalizar([1, 2, 2]) == [0.2, 0.4, 0.4]
+assert normalizar([1, 2, 5]) == [0.125, 0.25, 0.625]
+assert normalizar([1, 4, 5]) == [0.1, 0.4, 0.5]
+assert normalizar([6, 2, 2]) == [0.6, 0.2, 0.2]
+assert normalizar([]) == []
